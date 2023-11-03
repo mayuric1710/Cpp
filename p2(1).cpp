977268d687db494c0259adc6e953026557a8533a
mayuric1710 <148526248+mayuric1710@users.noreply.github.com>
2023-11-03 22:29:56 +0530

    p2(1).cpp

--- p2(1).cpp
+++ p2(1).cpp
@@ -8,8 +8,8 @@ int main(){
 
 		int a=10;
 		
-		cout<<"Janvi k. Chavda"<<endl;
-		cout<<"220130318090"<<endl;
+		cout<<"Mayuri R Chavda"<<endl;
+		cout<<"220130318091"<<endl;
 
 		cout<< "value of a as a global variable is "<<a<<endl;
 		cout<< "value of a as a local variable  is "<<::a<<endl;

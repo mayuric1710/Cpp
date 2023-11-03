19ef395ebc99d00a111803e7429723b52f1b43e2
mayuric1710 <148526248+mayuric1710@users.noreply.github.com>
2023-11-03 23:02:33 +0530

    p6(1).cpp

--- p6(1).cpp
+++ p6(1).cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main() {
-    cout<< "Janvi K.Chavda" << endl; 
-    cout << "220130318090" << endl;
+    cout << "Mayuri R. Chavda"<<endl;
+        cout << "220130318091"<<endl;
     ofstream outfile("test.txt");
     if (outfile.is_open()) {
         outfile << "This is a test." << endl;

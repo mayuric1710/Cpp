7098316e0372bb090d9030902a4888276ad5d8f3
mayuric1710 <148526248+mayuric1710@users.noreply.github.com>
2023-11-03 22:35:47 +0530

    p2(4) .cpp

--- p2(4) .cpp
+++ p2(4) .cpp
@@ -7,8 +7,8 @@ int main(){
 	
 	  float PI=3.14285;
 	  
-	  cout<<"Janvi k. chavda"<<endl;
-	  cout<<"220130318090"<<endl;
+	  cout << "Mayuri R. Chavda"<<endl;
+          cout << "220130318091"<<endl;
 	  
 	  cout<<"value of PI is ::"<<PI<<endl;
 	  cout<<"after applying precision::"<<setprecision(3)<<PI;

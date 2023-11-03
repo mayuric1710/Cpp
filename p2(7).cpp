586c8b90aa15f666d7fdc84722ee79f6a4bf214b
mayuric1710 <148526248+mayuric1710@users.noreply.github.com>
2023-11-03 22:37:15 +0530

    p2(7).cpp

--- p2(7).cpp
+++ p2(7).cpp
@@ -4,8 +4,8 @@ using namespace std;
 enum AND{FALSE,TRUE};
 int main()
 {
-	cout<<"Janvi k. Chavda"<<endl;
-	cout<<"220130318090"<<endl;
+	cout << "Mayuri R. Chavda"<<endl;
+        cout << "220130318091"<<endl;
 	cout<<"A"<<"\t"<<"B"<<"\t"<<"Y"<<endl;
 	cout<<FALSE<<"\t"<<FALSE<<"\t"<<FALSE*FALSE<<endl;
 	cout<<FALSE<<"\t"<<TRUE<<"\t"<<FALSE*TRUE<<endl;
